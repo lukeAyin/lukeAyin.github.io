2827ba3b4f9741eceeedb485c104fc077479640f
lukeAyin <lukeayin@gmail.com>
2023-05-15 22:50:48 +0800

    update find_dead.cc

--- note/huawei/week3/3.find_deadlock.cc
+++ note/huawei/week3/3.find_deadlock.cc
@@ -13,43 +13,47 @@ vector<vector<int>> occupied(MAXID,vector<int>());//进程占用的资源列表
 vector<vector<int>> waited(MAXID,vector<int>());//进程等待的资源列表
 map<int,int> res_map;//资源被占用的进程ID
 vector<vector<int>> need(MAXRES,vector<int>());//需要该资源的进程ID列表
-set<int> deadlock;
-void GetByRes(set<int>& res_occupied){//从占用的资源来找到需要所有这些资源的进程ID
-    for(auto it=res_occupied.begin();it!=res_occupied.end();it++){
-        deadlock.insert(need[*it].begin(),need[*it].end());
-    }
-}
+int id_visited[MAXID];//记录进程状态
 void GetDeadLockList(){
-    queue<int> q;
-    set<int> id_visited;//存储已经访问过的进程ID
+    memset(id_visited,0,sizeof(id_visited));
     for(int i=0;i<MAXID;i++){
-        if(id_visited.count(i)){
+        if(id_visited[i]>1||waited[i].size()==0){//已经访问过了or不可能死锁
             continue;
         }
-        set<int> res_occupied;//被占用的资源
+        queue<int> q;
+        q.push(i);
+        id_visited[i]=1;
         while (!q.empty())
         {
             int id=q.front();
             q.pop();
-            for(auto k:occupied[id]){
-                res_occupied.insert(k);
-                if(id_visited.count(res_map[k])){
-                    continue;
-                }
-                id_visited.insert(res_map[k]);
-                q.push(res_map[k]);
-            }
             for(auto k:waited[id]){
-                if(res_occupied.count(k)){//说明有环
-                    GetByRes(res_occupied);
+                if(res_map.find(k)!=res_map.end()){//有占用该资源的
+                    int nid=res_map[k];
+                    if(id_visited[nid]==3||id_visited[nid]==1){//死锁
+                        for(int j=0;j<MAXID;j++){
+                            if(id_visited[j]==1){
+                                id_visited[j]=3;
+                            }
+                        }
+                    }else if(id_visited[nid]==0){
+                        id_visited[nid]=1;
+                        q.push(nid);
+                    }
                 }
             }
         }
+        for(int j=0;j<MAXID;j++){
+            if(id_visited[j]==1){
+                id_visited[j]=2;
+            }
+        }
     }
-    for(auto it=deadlock.begin();it!=deadlock.end();it++){
-        printf("%d",*it);
+    for(int j=0;j<MAXID;j++){
+        if(id_visited[j]==3){
+            printf("%d ",j);
+        }
     }
-    
 }
 int main(){
     fstream input("3.find_deadlock.txt");
@@ -63,9 +67,9 @@ int main(){
         int ed1=str.find(')');
         int st2=str.find('(',ed1)+1;
         int ed2=str.find(')',st2);
-        // cout<<str.substr(0,st1-1)<<endl;
+        cout<<str.substr(0,st1-1)<<endl;
         int id=stoi(str.substr(0,st1-1));
-        // cout<<str.substr(st1,ed1-st1)<<" "<<str.substr(st2,ed2-st2)<<endl;
+        cout<<str.substr(st1,ed1-st1)<<" "<<str.substr(st2,ed2-st2)<<endl;
         istringstream iss1(str.substr(st1,ed1-st1));
         string tmp;
         while (iss1>>tmp)
